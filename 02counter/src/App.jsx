86cba94a2fbb5e608d2b50c8eb4dc7bd246e3fc5
Manjit Gupta <manjitgupta@2816gmail.com>
2026-08-08 06:33:44 +0530

    interview

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -11,16 +11,20 @@ function App() {
   // let counter =5
 
   const addValue = () =>{
-    if(counter<20){
-      setCounter(counter+1)
-    }
+    // if(counter<20){
+    //   setCounter(counter+1)
+    // }
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
     // console.log("clicked",counter)
   }
 
   const removeValue=() =>{
-    if(counter>0){
-      setCounter(counter-1)
-    }
+    // if(counter>0){
+      setCounter(prevCounter => prevCounter - 1)
+    // }
   }
 
   return (
